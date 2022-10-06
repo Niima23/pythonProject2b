096330d3e055d1330d697bd4da0c3bb19330cfb2
Niima23 <114267273+Niima23@users.noreply.github.com>
2022-10-05 23:53:45 -0700

    Update temp_convert.py

--- temp_convert.py
+++ temp_convert.py
@@ -1,3 +1,6 @@
+# Author: Amiin Samatar
+# GitHub username: Niima23
+# Date: 9/27/2022
 #python program to convert temperature in celsius
 C=float(input("Enter a temperature in celsius: "))
 #convert to Fahrenheit
